70499884dfc49913becb4ad1875af482c3e27236
Amirx <106537583+Awmiroosen@users.noreply.github.com>
2025-11-14 16:47:05 +0330

    besmellah first commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="w-full h-svh flex justify-center items-center">
+      <h1 className="text-2xl font-bold">404 - Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
